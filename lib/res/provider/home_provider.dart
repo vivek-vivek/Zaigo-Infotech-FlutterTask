a9697026e0c73d61c45b434da253bd128d5400cd
vivek-vivek <codemygallery@gmail.com>
2023-05-10 16:47:51 +0530

    erorr #2  tryed to make some new functions

--- lib/res/provider/home_provider.dart
+++ lib/res/provider/home_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:zaigo_infotech_flutter_task/model/lawyers_model.dart';
 
@@ -22,12 +24,18 @@ class HomeProvider with ChangeNotifier {
 // get datas drom api and update ui
   void addLawyerRespone(Datum lawyer) {
     lawyers.add(lawyer);
-
+    log(lawyer.id.toString());
     notifyListeners();
   }
 
   void changeloadingState(bool newState) {
     loadingState = newState;
     notifyListeners();
+  }
+
+  logLawyersList() {
+    for (var element in lawyers) {
+      log("${element.id} ${element.name} ");
+    }
   }
 }
